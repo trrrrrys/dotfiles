76596069b1a5c93e57684269ec6804b54b5b8381
trrrrrys <tsukahararu@gmail.com>
2023-04-07 13:05:34 +0900

    [update] add init.sh

---
+++ init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+func_prefix="init_"
+
+function init_zsh() {
+	# zplug install
+	if [ -z `command -v zplug` ]; then
+		curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh;
+	fi
+	# create symbolic link 
+	if ! [ -z `command -v dirname` ]; then
+		ln -sf $(cd $(dirname $0) && pwd)/zsh/.zshrc ~/
+	fi
+}
+
+func_list=`
+	grep -Eo 'function [a-zA-Z0-9_]+ *\(\)' "$0" | \
+ 		sed -E 's/function? //; s/ *\(\)//'
+`
+
+if [ -z "$1" ]; then
+  echo "keyword: "
+	echo $func_list
+  exit 0
+fi
+
+
+for arg in "$@"
+do
+	for func_name in $func_list; do
+		if [ "$func_prefix$arg" == "$func_name" ]; then
+			"$func_prefix$arg" 
+			break
+		fi
+	done
+done
