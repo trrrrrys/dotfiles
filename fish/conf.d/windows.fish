f3cfad525837dd3501024489a0040125104ef2f2
trrrrrys <tsukahara-ryo@dmm.com>
2023-04-17 22:50:51 +0900

    goodbye fish

--- fish/conf.d/windows.fish
+++
@@ -1,8 +0,0 @@
-
-# wslのみ実行
-which wsl.exe &> /dev/null
-if [ $status = 0 ]
-	set -x DISPLAY (ip route | grep 'default via' | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'):0
-	alias pbpaste="xsel --clipboard --output"
-	alias pbcopy="xsel --clipboard --input"
-end
